d2f2cf1f5964ae076b6b0787bb8ffc6a1ebd9211
Muhumuza Brian <muhumuzabr@gmail.com>
2018-07-26 22:40:02 +0300

    [task] import app configurations

--- application/__init__.py
+++ application/__init__.py
@@ -2,3 +2,5 @@ from flask import Flask, Blueprint
 from flask_restplus import Api
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+
+from application.config import configuration
