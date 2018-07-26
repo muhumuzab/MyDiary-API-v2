a614e3fd54e66d6d205cd121a0ff16dafc50a292
Muhumuza Brian <muhumuzabr@gmail.com>
2018-07-26 23:27:49 +0300

    [task] import datetime

--- application/views/entry_views.py
+++ application/views/entry_views.py
@@ -1,3 +1,4 @@
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restplus import Resource, Namespace, fields
 from flask import request, jsonify
+from datetime import datetime
