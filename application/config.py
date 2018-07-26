f2d489f1f0b920805bf79363dd256dbb1447e79e
Muhumuza Brian <muhumuzabr@gmail.com>
2018-07-27 00:01:33 +0300

    [fix] remove unnecessary fields from config class

--- application/config.py
+++ application/config.py
@@ -7,9 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     JWT_ALGORITHM = 'HS256'
-    JWT_SECRET_KEY = 'You and me knows very well it is secret'
-    JWT_BLACKLIST_ENABLED = True
-    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
+    JWT_SECRET_KEY = 'harryporter123'
     PASSWORD = 'annet'
     HOST = 'localhost'
     USER = 'postgres'
