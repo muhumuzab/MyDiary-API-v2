9d14736a17e33cc0b44220f4ca529b29a9cc2448
Muhumuza Brian <muhumuzabr@gmail.com>
2018-07-26 23:28:15 +0300

    [task] import diary entry model

--- application/views/entry_views.py
+++ application/views/entry_views.py
@@ -2,3 +2,5 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restplus import Resource, Namespace, fields
 from flask import request, jsonify
 from datetime import datetime
+
+from application.models.entry_models import DiaryEntry
