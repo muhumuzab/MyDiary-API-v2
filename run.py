cb47e1ed037ac7baaeba06d3a2de646bb440ecb6
Muhumuza Brian <muhumuzabr@gmail.com>
2018-07-26 23:59:10 +0300

    remove unnecessary import

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 import os
-from werkzeug.contrib.fixers import ProxyFix  # fix no spec in heroku
 
 from application import create_app
 
 if __name__ == '__main__':
     app = create_app('development')
-    app.wsgi_app = ProxyFix(app.wsgi_app)
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
