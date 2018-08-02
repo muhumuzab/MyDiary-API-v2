463c6ddd54e869d923df9b8aef09c9d20f7874e2
Muhumuza Brian <muhumuzabr@gmail.com>
2018-08-02 13:51:44 +0300

    update procfile

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from application import create_app
+app = create_app('development')
 
 if __name__ == '__main__':
-    app = create_app('development')
+    # app = create_app('development')
     app.run()
